5aedf4196fcf66e24b413dcf6e6f745e69261c34
Afeez G Lawal <afeezlg@gmail.com>
2023-09-14 14:08:35 +0100

    chore: add navbar

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const links = [
+  {
+    name: 'Benefits',
+    href: '/#benefits',
+  },
+  {
+    name: 'Survery types',
+    href: '/#survey-types',
+  },
+  {
+    name: 'Use Cases',
+    href: '/#use-cases',
+  },
+  {
+    name: 'Blog',
+    href: '/',
+  },
+  {
+    name: 'FAQ',
+    href: '/#faq',
+  },
+];
+
+export default function Nav() {
+  return (
+    <div className='flex justify-between items-center w-full px-20 py-4'>
+      <Link href='/'>
+        <Image src='/logo.svg' width={133} height={29} alt='Integraflow' />
+      </Link>
+
+      <div>
+        <ul className='flex space-x-8'>
+          {links.map((link) => (
+            <li key={link.name}>
+              <Link
+                href={link.href}
+                className='text-primary text-sm font-medium hover:underline'
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className='text-white text-sm font-normal bg-IFPurple rounded-lg px-8 py-[14px]'>
+        Join waitlist
+      </button>
+    </div>
+  );
+}

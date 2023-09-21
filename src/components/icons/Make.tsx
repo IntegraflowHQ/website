f95f6f50eb30ea7c9fafd46cfd1df22bb4beec0e
Afeez G Lawal <afeezlg@gmail.com>
2023-09-21 14:17:08 +0100

    chore: add icons

---
+++ src/components/icons/Make.tsx
@@ -0,0 +1,136 @@
+export const Make = () => {
+  return (
+    <svg
+      width='52'
+      height='51'
+      viewBox='0 0 52 51'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect
+        x='0.992188'
+        width='50.2854'
+        height='50.2854'
+        rx='14'
+        fill='white'
+      />
+      <g clip-path='url(#clip0_1674_6662)'>
+        <mask
+          id='mask0_1674_6662'
+          style={{ maskType: 'luminance' }}
+          maskUnits='userSpaceOnUse'
+          x='32'
+          y='16'
+          width='7'
+          height='21'
+        >
+          <path
+            d='M32.9281 16.5271H37.9079C38.2871 16.5271 38.5958 16.8362 38.5958 17.216V35.6272C38.5958 36.007 38.2871 36.3161 37.9079 36.3161H32.9281C32.5489 36.3161 32.2402 36.007 32.2402 35.6272V17.216C32.2402 16.8362 32.5489 16.5271 32.9281 16.5271Z'
+            fill='white'
+          />
+        </mask>
+        <g mask='url(#mask0_1674_6662)'>
+          <path
+            d='M37.8351 14.4603L25.8262 18.895L33.0005 38.382L45.0094 33.9473L37.8351 14.4603Z'
+            fill='url(#paint0_linear_1674_6662)'
+          />
+        </g>
+        <mask
+          id='mask1_1674_6662'
+          style={{ maskType: 'luminance' }}
+          maskUnits='userSpaceOnUse'
+          x='9'
+          y='16'
+          width='16'
+          height='21'
+        >
+          <path
+            d='M18.3201 16.7702L10.0656 33.2206C9.89507 33.5606 10.0318 33.9743 10.3713 34.145L14.8205 36.384C15.16 36.5548 15.573 36.4179 15.7435 36.0778L23.998 19.6275C24.1685 19.2874 24.0318 18.8738 23.6923 18.703L19.2432 16.464C19.1432 16.4139 19.0388 16.3904 18.9345 16.3904C18.6832 16.3904 18.4406 16.5288 18.3201 16.7702Z'
+            fill='white'
+          />
+        </mask>
+        <g mask='url(#mask1_1674_6662)'>
+          <path
+            d='M21.9201 11.1844L2.55859 19.5676L12.1405 41.7652L31.5021 33.3819L21.9201 11.1844Z'
+            fill='url(#paint1_linear_1674_6662)'
+          />
+        </g>
+        <mask
+          id='mask2_1674_6662'
+          style={{ maskType: 'luminance' }}
+          maskUnits='userSpaceOnUse'
+          x='21'
+          y='16'
+          width='11'
+          height='21'
+        >
+          <path
+            d='M25.2639 16.8289L21.7319 34.7602C21.6584 35.1326 21.8995 35.4933 22.2714 35.5683L27.1527 36.5546C27.526 36.6297 27.8905 36.3868 27.964 36.0129L31.496 18.0816C31.5695 17.7092 31.3284 17.3485 30.9566 17.2735L26.0753 16.2872C26.0297 16.2783 25.9841 16.2739 25.9386 16.2739C25.6167 16.2739 25.3286 16.5006 25.2639 16.8289Z'
+            fill='white'
+          />
+        </mask>
+        <g mask='url(#mask2_1674_6662)'>
+          <path
+            d='M30.3858 13.0565L15.0684 18.7039L22.8413 39.8509L38.1588 34.2035L30.3858 13.0565Z'
+            fill='url(#paint2_linear_1674_6662)'
+          />
+        </g>
+      </g>
+      <defs>
+        <linearGradient
+          id='paint0_linear_1674_6662'
+          x1='25.9401'
+          y1='29.6086'
+          x2='39.384'
+          y2='24.6591'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#B02DE9' />
+          <stop offset='0.02' stop-color='#B02DE9' />
+          <stop offset='0.8' stop-color='#6D00CC' />
+          <stop offset='1' stop-color='#6D00CC' />
+        </linearGradient>
+        <linearGradient
+          id='paint1_linear_1674_6662'
+          x1='14.2452'
+          y1='27.4881'
+          x2='28.6988'
+          y2='21.249'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#FF00FF' />
+          <stop offset='0.17' stop-color='#E90CF9' />
+          <stop offset='0.54' stop-color='#C023ED' />
+          <stop offset='0.73' stop-color='#B02DE9' />
+          <stop offset='1' stop-color='#B02DE9' />
+        </linearGradient>
+        <linearGradient
+          id='paint2_linear_1674_6662'
+          x1='11.0236'
+          y1='32.3518'
+          x2='40.6487'
+          y2='21.4627'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#FF00FF' />
+          <stop offset='0.02' stop-color='#FF00FF' />
+          <stop offset='0.09' stop-color='#E90CF9' />
+          <stop offset='0.23' stop-color='#C023ED' />
+          <stop offset='0.3' stop-color='#B02DE9' />
+          <stop offset='0.42' stop-color='#A42BE3' />
+          <stop offset='0.63' stop-color='#8626D5' />
+          <stop offset='0.85' stop-color='#6021C3' />
+          <stop offset='1' stop-color='#6021C3' />
+        </linearGradient>
+        <clipPath id='clip0_1674_6662'>
+          <rect
+            width='32'
+            height='26.8108'
+            fill='white'
+            transform='translate(9.99219 10)'
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};

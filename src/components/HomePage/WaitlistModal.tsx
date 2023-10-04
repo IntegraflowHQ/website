167be8c8173c723d15c46e58e68413555906f2ff
Afeez G Lawal <afeezlg@gmail.com>
2023-10-04 12:07:48 +0100

    style: make waitlist modal fullscreen on mobile

--- src/components/HomePage/WaitlistModal.tsx
+++ src/components/HomePage/WaitlistModal.tsx
@@ -85,30 +85,29 @@ export default function WaitlistModal({ buttonProps }: Props) {
             backdropFilter: "blur(6px)",
           }}
         />
-        <Dialog.Content className="fixed left-[50%] top-[50%] z-30 max-h-[80vh] w-max -translate-x-1/2 -translate-y-1/2 rounded-t-3xl bg-IFBlack p-8 lg:p-12">
-          {!joined ? (
-            <>
-              <div className="mb-5 flex justify-end lg:mb-8">
-                <Dialog.Close>
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="text-white"
-                  >
-                    <path
-                      d="M6 18L18 6M6 6L18 18"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </Dialog.Close>
-              </div>
-
+        <Dialog.Content className="fixed inset-0 z-30 flex items-center justify-center bg-IFBlack p-8 md:inset-auto md:left-1/2 md:top-1/2 md:w-max md:-translate-x-1/2 md:-translate-y-1/2 md:rounded-t-3xl lg:p-12">
+          <div>
+            <div className="mb-5 flex justify-end lg:mb-8">
+              <Dialog.Close>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="text-white"
+                >
+                  <path
+                    d="M6 18L18 6M6 6L18 18"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </Dialog.Close>
+            </div>
+            {!joined ? (
               <div className="flex w-[80vw] max-w-[390px] flex-col gap-7 lg:gap-[35px]">
                 <header className="space-y-2 text-center">
                   <Dialog.Title className="text-3xl font-semibold leading-9 text-IFText300 md:text-[32px] md:leading-[48px]">
@@ -132,36 +131,36 @@ export default function WaitlistModal({ buttonProps }: Props) {
                   <Button text="Join now" />
                 </form>
               </div>
-            </>
-          ) : (
-            <div className="flex w-[80vw] max-w-max flex-col items-center gap-2">
-              <header className="flex flex-col items-center gap-2 text-center">
-                <Dialog.Title className="max-w-[320px] text-[32px] font-semibold leading-9 text-IFText300 md:text-3xl md:leading-[48px]">
-                  Thank you for joining our waitlist
-                </Dialog.Title>
-                <Dialog.Description className="max-w-[420px] text-sm font-normal leading-normal text-IFText md:text-base">
-                  We&apos;re here to help you make the most of Integraflow. Feel
-                  free to reach out to us
-                </Dialog.Description>
-              </header>
+            ) : (
+              <div className="flex w-[80vw] max-w-max flex-col items-center gap-2">
+                <header className="flex flex-col items-center gap-2 text-center">
+                  <Dialog.Title className="max-w-[320px] text-[32px] font-semibold leading-9 text-IFText300 md:text-3xl md:leading-[48px]">
+                    Thank you for joining our waitlist
+                  </Dialog.Title>
+                  <Dialog.Description className="max-w-[420px] text-sm font-normal leading-normal text-IFText md:text-base">
+                    We&apos;re here to help you make the most of Integraflow.
+                    Feel free to reach out to us
+                  </Dialog.Description>
+                </header>
 
-              <div className="grid w-full grid-cols-2 gap-3 p-8">
-                {socialLinks.map(({ name, icon: Icon, url }) => (
-                  <Link
-                    href={url}
-                    target="_blank"
-                    className="flex items-center justify-center gap-4 rounded-full bg-[#131313] p-4 sm:w-full sm:max-w-[206px] md:p-6 lg:justify-normal"
-                    key={name}
-                  >
-                    <Icon />
-                    <span className="hidden text-base font-medium text-[#CECBCB] sm:inline">
-                      {name}
-                    </span>
-                  </Link>
-                ))}
+                <div className="grid w-full grid-cols-2 gap-3 p-8">
+                  {socialLinks.map(({ name, icon: Icon, url }) => (
+                    <Link
+                      href={url}
+                      target="_blank"
+                      className="flex items-center justify-center gap-4 rounded-full bg-[#131313] p-4 sm:w-full sm:max-w-[206px] md:p-6 lg:justify-normal"
+                      key={name}
+                    >
+                      <Icon />
+                      <span className="hidden text-base font-medium text-[#CECBCB] sm:inline">
+                        {name}
+                      </span>
+                    </Link>
+                  ))}
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
           <Toaster />
         </Dialog.Content>
       </Dialog.Portal>
